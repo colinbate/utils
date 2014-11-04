feca71ffd922cfd60f6897bc08cb93b0d46758ad
Colin Bate <1060912+colinbate@users.noreply.github.com>
2014-11-04 10:57:44 -0400

    More owly goodness

--- owl-me/owl.js
+++ owl-me/owl.js
@@ -1,30 +1,103 @@
 (function ($, w) {
   var rest = 'url(http://colinbate.bitbucket.org/owl-me/owl.png)',
       fly = 'url(http://colinbate.bitbucket.org/owl-me/owl-flap.png)',
+      $w = $(window),
+      flap = function ($t) {
+        return setInterval(function () {
+          if ($t.hasClass('owl-flap')) {
+            $t.css('backgroundImage', rest);
+          } else {
+            $t.css('backgroundImage', fly);
+          }
+          $t.toggleClass('owl-flap');
+        }, 70);
+      },
+      randomInt = function (max) {
+        return Math.floor(Math.random() * max);
+      },
       flyOff = function (e) {
         var $t = $(e.target),
             top = $t.offset().top,
-            w = (Math.random() * $(window).width()) - 120,
-            flap = setInterval(function () {
-              if ($t.hasClass('owl-flap')) {
-                $t.css('backgroundImage', rest);
-              } else {
-                $t.css('backgroundImage', fly);
-              }
-              $t.toggleClass('owl-flap');
-            }, 70);
+            w = randomInt($w.width()) - 120,
+            flapRef = flap($t);
         $t.animate({ top: ['-150px', 'swing'], left: [w + 'px', 'swing'] },
           {
             duration: 1600 + (top * 2),
             complete: function () {
-              clearInterval(flap);
+              clearInterval(flapRef);
+              $t.remove();
             }
           }
         );
+      },
+      flyIn = function ($owl) {
+        var $t = $owl,
+            perch = $t.data('perch'),
+            w = randomInt($w.width()) - 120,
+            flapRef = flap($t);
+        $t.offset({top: -150, left: w}).show().animate({
+            top: [perch.top + 'px', 'swing'],
+            left: [perch.left + 'px', 'swing']
+          },
+          {
+            duration: 1600 + (perch.top * 2),
+            complete: function () {
+              clearInterval(flapRef);
+              $t.click(flyOff);
+            }
+          }
+        );
+      },
+      flyThrough = function ($owl, ww, wh) {
+        var flapRef = flap($owl);
+        $owl.offset({
+          top: randomInt(wh),
+          left: -120
+        }).show().animate({
+            top: [randomInt(wh) + 'px', 'swing'],
+            left: [ww + 'px', 'swing']
+          },
+          {
+            duration: ww * 4,
+            complete: function () {
+              clearInterval(flapRef);
+              $owl.remove();
+            }
+          })
+      },
+      flyMeThrough = function ($owl, ww, wh) {
+        return function () {
+          flyThrough($owl, ww, wh);
+        };
+      },
+      addOwl = function (id) {
+        var attachId = id || randomInt(100000);
+        return $('<div></div>').attr('id', 'owl-' + attachId).css({
+          position: 'absolute',
+          width: '120px',
+          height: '100px',
+          margin: 0,
+          padding: 0,
+          backgroundColor: 'transparent',
+          backgroundImage: rest,
+          backgroundRepeat: 'no-repeat',
+          zIndex: 2500,
+        }).hide().appendTo('body');
       };
 
   $.fn.owlMe = function () {
-    return this.each(function () {
+    var totalOwls = this.length,
+        ii, wh, ww;
+    if (!this.length) {
+      totalOwls = randomInt(5) + 3;
+      ww = $w.outerWidth() + 1;
+      wh = $w.outerHeight() - 100;
+      for (ii = 0; ii < totalOwls; ii += 1) {
+        setTimeout(flyMeThrough(addOwl(), ww, wh), (ii + 3) * 550);
+      }
+      return this;
+    }
+    return this.each(function (whichOwl) {
       var $t = $(this);
       if ($t.hasClass('owl-me')) {
         return;
@@ -33,18 +106,10 @@
       pos.top -= 94;
       pos.left += ($t.outerWidth() / 2) - 60;
       $t.addClass('owl-me');
-      var attachId = $t.attr('id') || Math.floor(Math.random() * 100000);
-      var $owl = $('<div></div>').attr('id', 'owl-' + attachId).css({
-        position: 'absolute',
-        width: '120px',
-        height: '100px',
-        margin: 0,
-        padding: 0,
-        backgroundColor: 'transparent',
-        backgroundImage: rest,
-        backgroundRepeat: 'no-repeat',
-        zIndex: 25,
-      }).click(flyOff).appendTo('body').offset(pos);
+      var $owl = addOwl($t.attr('id')).data('perch', pos);
+      setTimeout(function () {
+        flyIn($owl);
+      }, (whichOwl + 5) * 140);
     });
   };
   $('<div></div>').css({position:'absolute',top:'-1000px',backgroundImage:fly}).appendTo('body');
